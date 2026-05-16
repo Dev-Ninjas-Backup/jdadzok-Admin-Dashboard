705ad575b3491eee7c4ba961be221c29d9b1bc2f
MIRZA SAIKAT AHMMED <mirzasaikatahmmed@gmail.com>
2026-05-17 05:37:42 +0600

    Add allowedHosts to preview configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -13,5 +12,8 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  preview: {
+    allowedHosts: ['admin.synqulan.com'],
   },
 })
