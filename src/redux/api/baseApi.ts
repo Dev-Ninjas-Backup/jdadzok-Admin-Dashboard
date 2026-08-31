8ef6aeb0c111bf4a3053e7694fff3268feb8dfae
MIRZA SAIKAT AHMMED <mirzasaikatahmmed@gmail.com>
2026-08-31 10:20:19 +0600

    Remove dotenv dependency and update base API URL in baseApi.ts; add vite.config.js for configuration

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -4,10 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    // baseUrl: process.env.REACT_APP_API_URL || "http://13.204.75.28:5056/",
-    // baseUrl: "http://13.204.75.28:5056/",
-    // baseUrl: import.meta.env.VITE_API_URL || "http://212.85.26.187:5056/",
-    baseUrl: "https://api.synqulan.com/",
+    baseUrl: import.meta.env.VITE_API_URL || "https://api.synqulan.com/",
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("token"); // token persisted on login
       if (token) headers.set("Authorization", `Bearer ${token}`);
